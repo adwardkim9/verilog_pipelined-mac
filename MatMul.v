7bf26717dce386e83fc625cfb625e2a5c45cb68f
adwardkim9 <68454319+adwardkim9@users.noreply.github.com>
2024-06-05 16:19:22 +0900

    Add files via upload

--- MatMul.v
+++ MatMul.v
@@ -7,7 +7,7 @@ module MatMul #(
     input RSTN,
     input signed [39:0] Weight_i,
     input signed [39:0] In_i,
-    output reg signed [79:0] OUT_o,
+    output reg signed [39:0] OUT_o,
     output reg VAL_o,
     output reg OV_o
 );
@@ -27,8 +27,10 @@ module MatMul #(
 
 	reg signed [7:0] Vin1, Vin2, Vin3, Vin4, Vin5;
 	
-	reg signed [16*(T+4)-1:0] out_temp1, out_temp2, out_temp3, out_temp4, out_temp5;
-	reg signed [16*T-1:0] out_reg1, out_reg2, out_reg3, out_reg4, out_reg5;
+	reg signed [8*(T+3)-1:0] out_temp1, out_temp2, out_temp3, out_temp4, out_temp5;
+	reg signed [8*T-1:0] out_reg1, out_reg2, out_reg3, out_reg4, out_reg5;
+	
+    reg ov1, ov2, ov3, ov4, ov5;
  
     always @(posedge CLK or negedge RSTN) begin
         if (!RSTN)
@@ -91,7 +93,8 @@ module MatMul #(
 			input_reg1 <= 0; input_reg2 <= 0; input_reg3 <=0; input_reg4 <= 0; input_reg5 <= 0;
 			Vin1 <= 0; Vin2 <= 0; Vin3 <= 0; Vin4 <= 0; Vin5 <= 0;
 			out_temp1 <= 0; out_temp2 <= 0; out_temp3 <= 0; out_temp4 <= 0; out_temp5 <= 0;
-			out_reg1 <= 0; out_reg2 <= 0; out_reg3 <= 0; out_reg4 <= 0; out_reg5 <= 0;
+			out_reg1 <= 0; out_reg2 <= 0; out_reg3 <= 0; out_reg4 <= 0; out_reg5 <= 0; 
+			ov1 <= 0; ov2 <= 0; ov3 <= 0; ov4 <= 0; ov5 <=0;
 		end else if (present_state == LOAD_WEIGHT) begin
 			weight_temp[weight_counter] <= Weight_i;
 		end else if (present_state == LOAD_INPUT) begin
@@ -125,11 +128,11 @@ module MatMul #(
     end
 
     // Intermediate accumulation wires
-    wire [15:0] acc11, acc12, acc13, acc14, acc15;
-    wire [15:0] acc21, acc22, acc23, acc24, acc25;
-    wire [15:0] acc31, acc32, acc33, acc34, acc35;
-    wire [15:0] acc41, acc42, acc43, acc44, acc45;
-    wire [15:0] acc51, acc52, acc53, acc54, acc55;
+    wire [17:0] acc11, acc12, acc13, acc14, acc15;
+    wire [17:0] acc21, acc22, acc23, acc24, acc25;
+    wire [17:0] acc31, acc32, acc33, acc34, acc35;
+    wire [17:0] acc41, acc42, acc43, acc44, acc45;
+    wire [17:0] acc51, acc52, acc53, acc54, acc55;
 
     // Intermediate vertical input wires
     wire [7:0] vin11, vin12, vin13, vin14, vin15;
@@ -138,38 +141,31 @@ module MatMul #(
     wire [7:0] vin41, vin42, vin43, vin44, vin45;
     wire [7:0] vin51, vin52, vin53, vin54, vin55;
 	
-	// Intermediate vertical input wires
-    wire ov11, ov12, ov13, ov14, ov15;
-    wire ov21, ov22, ov23, ov24, ov25;
-    wire ov31, ov32, ov33, ov34, ov35;
-    wire ov41, ov42, ov43, ov44, ov45;
-    wire ov51, ov52, ov53, ov54, ov55;
-
-    MAC u11 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[0][39:32]), .In(Vin1), .partial_sum(16'b0), .vertical_out(vin11), .result(acc11));
+    MAC u11 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[0][39:32]), .In(Vin1), .partial_sum(18'b0), .vertical_out(vin11), .result(acc11));
     MAC u12 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[0][31:24]), .In(Vin2), .partial_sum(acc11), .vertical_out(vin12), .result(acc12));
     MAC u13 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[0][23:16]), .In(Vin3), .partial_sum(acc12), .vertical_out(vin13), .result(acc13));
     MAC u14 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[0][15:8]), .In(Vin4), .partial_sum(acc13), .vertical_out(vin14), .result(acc14));
     MAC u15 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[0][7:0]), .In(Vin5), .partial_sum(acc14), .vertical_out(vin15), .result(acc15));
 
-    MAC u21 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[1][39:32]), .In(vin11), .partial_sum(16'b0), .vertical_out(vin21), .result(acc21));
+    MAC u21 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[1][39:32]), .In(vin11), .partial_sum(18'b0), .vertical_out(vin21), .result(acc21));
     MAC u22 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[1][31:24]), .In(vin12), .partial_sum(acc21), .vertical_out(vin22), .result(acc22));
     MAC u23 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[1][23:16]), .In(vin13), .partial_sum(acc22), .vertical_out(vin23), .result(acc23));
     MAC u24 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[1][15:8]), .In(vin14), .partial_sum(acc23), .vertical_out(vin24), .result(acc24));
     MAC u25 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[1][7:0]), .In(vin15), .partial_sum(acc24), .vertical_out(vin25),.result(acc25));
 
-    MAC u31 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[2][39:32]), .In(vin21), .partial_sum(16'b0), .vertical_out(vin31), .result(acc31));
+    MAC u31 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[2][39:32]), .In(vin21), .partial_sum(18'b0), .vertical_out(vin31), .result(acc31));
     MAC u32 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[2][31:24]), .In(vin22), .partial_sum(acc31), .vertical_out(vin32), .result(acc32));
     MAC u33 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[2][23:16]), .In(vin23), .partial_sum(acc32), .vertical_out(vin33), .result(acc33));
     MAC u34 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[2][15:8]), .In(vin24), .partial_sum(acc33), .vertical_out(vin34), .result(acc34));
     MAC u35 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[2][7:0]), .In(vin25), .partial_sum(acc34), .vertical_out(vin35), .result(acc35));
 
-    MAC u41 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[3][39:32]), .In(vin31), .partial_sum(16'b0), .vertical_out(vin41), .result(acc41));
+    MAC u41 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[3][39:32]), .In(vin31), .partial_sum(18'b0), .vertical_out(vin41), .result(acc41));
     MAC u42 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[3][31:24]), .In(vin32), .partial_sum(acc41), .vertical_out(vin42), .result(acc42));
     MAC u43 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[3][23:16]), .In(vin33), .partial_sum(acc42), .vertical_out(vin43), .result(acc43));
     MAC u44 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[3][15:8]), .In(vin34), .partial_sum(acc43), .vertical_out(vin44), .result(acc44));
     MAC u45 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[3][7:0]), .In(vin35), .partial_sum(acc44), .vertical_out(vin45), .result(acc45));
 
-    MAC u51 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[4][39:32]), .In(vin41), .partial_sum(16'b0), .vertical_out(vin51), .result(acc51));
+    MAC u51 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[4][39:32]), .In(vin41), .partial_sum(18'b0), .vertical_out(vin51), .result(acc51));
     MAC u52 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[4][31:24]), .In(vin42), .partial_sum(acc51), .vertical_out(vin52), .result(acc52));
     MAC u53 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[4][23:16]), .In(vin43), .partial_sum(acc52), .vertical_out(vin53), .result(acc53));
     MAC u54 (.CLK(CLK), .RSTN(RSTN), .Weight(weight_temp[4][15:8]), .In(vin44), .partial_sum(acc53), .vertical_out(vin54), .result(acc54));
@@ -180,54 +176,53 @@ module MatMul #(
 			// store result value to out_temp, out_reg -> print OUT_o
 			case(N) // N = 3 : 6, N = 5 : 9
 				1: begin
-					out_temp1[16*(T+4)-1 - (compute_counter-N-3)*16 -: 16] <= acc11;
-					out_temp2[16*(T+4)-1 - (compute_counter-N-4)*18 -: 16] <= acc21;
-					out_temp3[16*(T+4)-1 - (compute_counter-N-5)*18 -: 16] <= acc31;
-					out_temp4[16*(T+4)-1 - (compute_counter-N-6)*18 -: 16] <= acc41;
-					out_temp5[16*(T+4)-1 - (compute_counter-N-7)*18 -: 16] <= acc51;
+					out_temp1[8*(T+4)-1 - (compute_counter-N-3)*8 -: 8] <= acc11[7:0]; ov1 <= |acc11[17:8];
+					out_temp2[8*(T+4)-1 - (compute_counter-N-4)*8 -: 8] <= acc21[7:0]; ov2 <= |acc21[17:8];
+					out_temp3[8*(T+4)-1 - (compute_counter-N-5)*8 -: 8] <= acc31[7:0]; ov3 <= |acc31[17:8];
+					out_temp4[8*(T+4)-1 - (compute_counter-N-6)*8 -: 8] <= acc41[7:0]; ov4 <= |acc41[17:8];
+					out_temp5[8*(T+4)-1 - (compute_counter-N-7)*8 -: 8] <= acc51[7:0]; ov5 <= |acc51[17:8];
 				end
 				2: begin
-					out_temp1[16*(T+4)-1 - (compute_counter-N-3)*16 -: 16] <= acc11;
-					out_temp2[16*(T+4)-1 - (compute_counter-N-4)*16 -: 16] <= acc22;
-					out_temp3[16*(T+4)-1 - (compute_counter-N-5)*16 -: 16] <= acc32;
-					out_temp4[16*(T+4)-1 - (compute_counter-N-6)*16 -: 16] <= acc42;
-					out_temp5[16*(T+4)-1 - (compute_counter-N-7)*16 -: 16] <= acc52;
+					out_temp1[8*(T+4)-1 - (compute_counter-N-3)*8 -: 8] <= acc11[7:0]; ov1 <= |acc12[17:8];
+					out_temp2[8*(T+4)-1 - (compute_counter-N-4)*8 -: 8] <= acc22[7:0]; ov2 <= |acc22[17:8];
+					out_temp3[8*(T+4)-1 - (compute_counter-N-5)*8 -: 8] <= acc32[7:0]; ov3 <= |acc32[17:8];
+					out_temp4[8*(T+4)-1 - (compute_counter-N-6)*8 -: 8] <= acc42[7:0]; ov4 <= |acc42[17:8];
+					out_temp5[8*(T+4)-1 - (compute_counter-N-7)*8 -: 8] <= acc52[7:0]; ov5 <= |acc52[17:8];
 				end
 				3: begin
-					out_temp1[16*(T+4)-1 - (compute_counter-N-3)*16 -: 16] <= acc13;
-					out_temp2[16*(T+4)-1 - (compute_counter-N-4)*16 -: 16] <= acc23;
-					out_temp3[16*(T+4)-1 - (compute_counter-N-5)*16 -: 16] <= acc33;
-					out_temp4[16*(T+4)-1 - (compute_counter-N-6)*16 -: 16] <= acc43;
-					out_temp5[16*(T+4)-1 - (compute_counter-N-7)*16 -: 16] <= acc53;
+					out_temp1[8*(T+4)-1 - (compute_counter-N-3)*8 -: 8] <= acc13[7:0]; ov1 <= |acc13[17:8];
+					out_temp2[8*(T+4)-1 - (compute_counter-N-4)*8 -: 8] <= acc23[7:0]; ov2 <= |acc23[17:8];
+					out_temp3[8*(T+4)-1 - (compute_counter-N-5)*8 -: 8] <= acc33[7:0]; ov3 <= |acc33[17:8];
+					out_temp4[8*(T+4)-1 - (compute_counter-N-6)*8 -: 8] <= acc43[7:0]; ov4 <= |acc43[17:8];
+					out_temp5[8*(T+4)-1 - (compute_counter-N-7)*8 -: 8] <= acc53[7:0]; ov5 <= |acc53[17:8];
 				end
 				4: begin
-					out_temp1[16*(T+4)-1 - (compute_counter-N-3)*16 -: 16] <= acc14;
-					out_temp2[16*(T+4)-1 - (compute_counter-N-4)*16 -: 16] <= acc24;
-					out_temp3[16*(T+4)-1 - (compute_counter-N-5)*16 -: 16] <= acc34;
-					out_temp4[16*(T+4)-1 - (compute_counter-N-6)*16 -: 16] <= acc44;
-					out_temp5[16*(T+4)-1 - (compute_counter-N-7)*16 -: 16] <= acc54;
+					out_temp1[8*(T+4)-1 - (compute_counter-N-3)*8 -: 8] <= acc14[7:0]; ov1 <= |acc14[17:8];
+					out_temp2[8*(T+4)-1 - (compute_counter-N-4)*8 -: 8] <= acc24[7:0]; ov2 <= |acc24[17:8];
+					out_temp3[8*(T+4)-1 - (compute_counter-N-5)*8 -: 8] <= acc34[7:0]; ov3 <= |acc34[17:8];
+					out_temp4[8*(T+4)-1 - (compute_counter-N-6)*8 -: 8] <= acc44[7:0]; ov4 <= |acc44[17:8];
+					out_temp5[8*(T+4)-1 - (compute_counter-N-7)*8 -: 8] <= acc54[7:0]; ov5 <= |acc54[17:8];
 				end
 				5: begin
-					out_temp1[16*(T+4)-1 - (compute_counter-N-3)*16 -: 16] <= acc15;
-					out_temp2[16*(T+4)-1 - (compute_counter-N-4)*16 -: 16] <= acc25;
-					out_temp3[16*(T+4)-1 - (compute_counter-N-5)*16 -: 16] <= acc35;
-					out_temp4[16*(T+4)-1 - (compute_counter-N-6)*16 -: 16] <= acc45;
-					out_temp5[16*(T+4)-1 - (compute_counter-N-7)*16 -: 16] <= acc55;
+					out_temp1[8*(T+4)-1 - (compute_counter-N-3)*8 -: 8] <= acc15[7:0]; ov1 <= |acc15[17:8];
+					out_temp2[8*(T+4)-1 - (compute_counter-N-4)*8 -: 8] <= acc25[7:0]; ov2 <= |acc25[17:8];
+					out_temp3[8*(T+4)-1 - (compute_counter-N-5)*8 -: 8] <= acc35[7:0]; ov3 <= |acc35[17:8];
+					out_temp4[8*(T+4)-1 - (compute_counter-N-6)*8 -: 8] <= acc45[7:0]; ov4 <= |acc45[17:8];
+					out_temp5[8*(T+4)-1 - (compute_counter-N-7)*8 -: 8] <= acc55[7:0]; ov5 <= |acc55[17:8];
 				end
 			endcase
 		end
     end
 	
 	always @(posedge CLK) begin
-	
         if (present_state == COMPUTE) begin
 			if(compute_counter >= N+8) begin
 				VAL_o <= 1;
-				OUT_o <= {out_temp1[16*(T+4)-1 - (compute_counter-N-8)*16 -: 16],
-							out_temp2[16*(T+4)-1 - (compute_counter-N-8)*16 -: 16], 
-							out_temp3[16*(T+4)-1 - (compute_counter-N-8)*16 -: 16],
-							out_temp4[16*(T+4)-1 - (compute_counter-N-8)*16 -: 16],
-							out_temp5[16*(T+4)-1 - (compute_counter-N-8)*16 -: 16]};
+				OUT_o <= {out_temp1[8*(T+4)-1 - (compute_counter-N-8)*8 -: 8],
+							out_temp2[8*(T+4)-1 - (compute_counter-N-8)*8 -: 8], 
+							out_temp3[8*(T+4)-1 - (compute_counter-N-8)*8 -: 8],
+							out_temp4[8*(T+4)-1 - (compute_counter-N-8)*8 -: 8],
+							out_temp5[8*(T+4)-1 - (compute_counter-N-8)*8 -: 8]};
 			end
 		end
     end
@@ -238,6 +233,7 @@ module MatMul #(
             VAL_o <= 0;
             OV_o <= 0;
         end else if (present_state == COMPUTE) begin
+			OV_o <= ov1 | ov2 | ov3 | ov4 | ov5;
 			if(present_state == IDLE) VAL_o <= 0;
 		end
 	end
@@ -248,17 +244,19 @@ module MAC (
     input RSTN,
     input [7:0] Weight,
     input [7:0] In,
-    input [15:0] partial_sum,
+    input [17:0] partial_sum,
     output reg [7:0] vertical_out,
-    output reg [15:0] result
+    output reg [17:0] result
 );
+	reg [15:0] mult_result;
 
     always @(posedge CLK or negedge RSTN) begin
         if (!RSTN)
             result <= 0;
         else
             vertical_out <= In;
-            result <= partial_sum + (Weight * In);
+			mult_result <= Weight * In;
+            result <= partial_sum + {2'b0, mult_result};
     end
 
 endmodule
